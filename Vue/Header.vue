0671e8ef2929949fd33045f796f6df7856d76d7d
zl454 <531720074@qq.com>
2020-10-18 23:38:14 +0800

    Vue

---
+++ Vue/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1 @click="changeTitle">{{title}}</h1>//绑定一个点击事件
+  </header>
+</template>
+<script>
+export default {
+  name: 'app-header',
+  data() {
+    return {
+      title:"Vue.js Demo"
+    }
+  },
+  methods:{
+    changeTitle() {
+      this.$emit("titleChanged","子向父组件传值");//自定义事件  传递值“子向父组件传值”
+    }
+  }
+}
+</script>
